97a162abd0854b38dfa033af2903f5f3b1ba5725
YD <eeegordolinskiy@gmail.com>
2026-04-23 00:56:59 +0200

    fix: remove duplicate WalletMultiButton import

--- apps/web/src/components/LandingPage.tsx
+++ apps/web/src/components/LandingPage.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Link from 'next/link'
-import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import dynamic from 'next/dynamic'
 import { useState } from 'react'
 import { usePots } from '@/hooks/usePots'
